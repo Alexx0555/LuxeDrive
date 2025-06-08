ea877d6b704f71bfa349ded6d4749b60a16246ca
Robin00755 <azhagesh2020@gmail.com>
2025-06-08 17:04:59 +0530

    img updates

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -4,16 +4,16 @@ import CarCard from '../card/CarCard';
 import './homepage.css';
 
 const carData = [
-    { id: 1, brand: 'Mercedes', model: 'S-Class', price: 110000, image: './src/assets/s-class.webp' },
-    { id: 2, brand: 'BMW', model: '7 Series', price: 105000, image: './src/assets/bmw-7.webp' },
-    { id: 3, brand: 'Audi', model: 'A8', price: 100000, image: './src/assets/a8.webp'},
-    { id: 4, brand: 'Porsche', model: '911 Carrera', price: 125000, image: './src/assets/911.webp' },
-    { id: 5, brand: 'Mercedes', model: 'G-Wagon', price: 140000, image: './src/assets/wag.jpg' },
-    { id: 6, brand: 'Rolls-Royce', model: 'Phantom', price: 450000, image: './src/assets/rrp.jpg' },
-    { id: 7, brand: 'BMW', model: 'X7', price: 85000, image: './src/assets/x7.jpg' },
-    { id: 8, brand: 'Audi', model: 'Q8', price: 80000, image: './src/assets/q8.jpg' },
-    { id: 9, brand: 'Porsche', model: 'Taycan', price: 150000, image: './src/assets/taycan.jpeg' },
-    { id: 10, brand: 'Rolls-Royce', model: 'Cullinan', price: 350000, image: './src/assets/cull.jpg' },
+    { id: 1, brand: 'Mercedes', model: 'S-Class', price: 110000, image: './s-class.webp' },
+    { id: 2, brand: 'BMW', model: '7 Series', price: 105000, image: './bmw-7.webp' },
+    { id: 3, brand: 'Audi', model: 'A8', price: 100000, image: './a8.webp'},
+    { id: 4, brand: 'Porsche', model: '911 Carrera', price: 125000, image: './911.webp' },
+    { id: 5, brand: 'Mercedes', model: 'G-Wagon', price: 140000, image: './wag.jpg' },
+    { id: 6, brand: 'Rolls-Royce', model: 'Phantom', price: 450000, image: './rrp.jpg' },
+    { id: 7, brand: 'BMW', model: 'X7', price: 85000, image: './x7.jpg' },
+    { id: 8, brand: 'Audi', model: 'Q8', price: 80000, image: './q8.jpg' },
+    { id: 9, brand: 'Porsche', model: 'Taycan', price: 150000, image: './taycan.jpeg' },
+    { id: 10, brand: 'Rolls-Royce', model: 'Cullinan', price: 350000, image: './cull.jpg' },
 ];
 
 const HomePage = ({ addToCart }) => {
